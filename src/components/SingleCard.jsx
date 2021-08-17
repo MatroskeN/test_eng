d7d1ee8903debc9acdb54de7263cd0bc377f9c56
Matrosken <piranya1399@gmail.com>
2021-08-18 00:14:43 +0400

    уау

--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -1,30 +1,38 @@
 import home_pic from "../home-pic.png";
 
-function SingleCard(props) {
-    return (
-        <div className='cards__item'>
-            <div className="home__pic">
-                <img className="home__img" src={home_pic} alt="home_pic"/>
-                <div className="home__label">
-                    {props.type}
+function SingleCard(card) {
+
+        return (
+            <div className='cards__item'>
+                <div className="home__pic">
+                    <img className="home__img" src={home_pic} alt="home_pic"/>
+                    {card.type === "IndependentLiving" ? (
+                        <div className="home__label">
+                            Independent living
+                        </div>
+                    ) : (
+                        <div className="home__label orange">
+                            Restaurant & Support available
+                        </div>
+                    )}
+                </div>
+                <div className="home__info">
+                    <p className="home__title">
+                        {card.title}
+                    </p>
+                    <p className="home__address">
+                        {card.address}
+                    </p>
+                    <p className="home__price">
+                        New Properties for Sale from <strong>£{card.price}</strong>
+                    </p>
+                    <p className="home__note">
+                        Shared Ownership Available
+                    </p>
                 </div>
             </div>
-            <div className="home__info">
-                <p className="home__title">
-                    {props.title}
-                </p>
-                <p className="home__address">
-                    {props.address}
-                </p>
-                <p className="home__price">
-                    New Properties for Sale from <strong>£{props.price}</strong>
-                </p>
-                <p className="home__note">
-                    Shared Ownership Available
-                </p>
-            </div>
-        </div>
-    )
+        )
+
 }
 
 export default SingleCard;
